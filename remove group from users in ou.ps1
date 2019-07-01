c644d7aad5b416b72a3f608d4f5ecbacd84963f7
Vilma Hallikas <42251794+VilmaH@users.noreply.github.com>
2019-07-01 09:58:26 +0300

    Create remove group from users in ou.ps1

---
+++ remove group from users in ou.ps1
@@ -0,0 +1,18 @@
+﻿#This script removes GROUP group from users that are in the OUs defined in $sb.
+
+$logfile = "logfile.log"
+$sb = "OU=Disabled,DC=example,DC=com","OU=Deleted,DC=example,DC=com" 
+
+foreach($search in $sb){
+    
+    $users = Get-AdUser -SearchBase $search -Filter *
+
+    foreach($user in $users){
+
+        if(Get-ADPrincipalGroupMembership -Identity $user | Select Name | Where-Object { $_ -like '*GROUP*' }){
+
+            Remove-ADGroupMember -Identity GROUP -Members $user -Confirm:$false
+            Add-Content $logFile "$(Get-Date -format u) - $user"  -Encoding UTF8
+        }
+    }
+}
