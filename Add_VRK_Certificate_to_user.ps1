5f28ca32f8f75fdf34117bc717180d2c582558d1
Vilma Hallikas <42251794+VilmaH@users.noreply.github.com>
2020-02-05 11:54:37 +0200

    Update Add_VRK_Certificate_to_user.ps1
    Improved error handling

--- Add_VRK_Certificate_to_user.ps1
+++ Add_VRK_Certificate_to_user.ps1
@@ -1,23 +1,27 @@
-﻿#
+#
 # Fetches user's certificate from VRK LDAP and installs it in local AD under user object
 #
 # Requires https://gallery.technet.microsoft.com/scriptcenter/Using-SystemDirectoryServic-0adf7ef5#content
 
-    param(
-        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
-        [string]$serial,
-        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
-        [string]$employeeid
+param(
+    [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+    [string]$serial,
+    [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+    [int]$employeeid
 )
 
 
-    process {
-    Write-Host $serial
+process {
 
-#.net object for handling bytearray to windows cert object
-$Certobject = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
+    if($employeeid -gt "160000")
+    {
+        Write-Error "Employeeid number too big" -ErrorAction Continue
+    }else{
 
-$Ldapconnection = Get-LdapConnection -LdapServer "ldap.fineid.fi:389" -AuthType Anonymous
+        #.net object for handling bytearray to windows cert object
+        $Certobject = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
+
+        $Ldapconnection = Get-LdapConnection -LdapServer "ldap.fineid.fi:389" -AuthType Anonymous
  
         $cn = Find-LDAPObject -LdapConnection $ldapconnection -searchFilter:"(serialnumber=$serial)" -searchBase:"c=FI" -searchScope Subtree
 
@@ -54,32 +58,27 @@ $Ldapconnection = Get-LdapConnection -LdapServer "ldap.fineid.fi:389" -AuthType 
             }
             else{
             
-            try{
                 $dn = get-aduser -filter {employeeid -eq $employeeid} | select distinguishedname
-                $dn = $dn.distinguishedname.Split(",")
-                [array]::Reverse($dn)
-                $dn = [system.String]::Join(",", $dn)
+                if($dn -eq $null){
+                    Write-Error "User $employeeid not found in AD" -ErrorAction Continue
+                }else{
+                    $dn = $dn.distinguishedname.Split(",")
+                    [array]::Reverse($dn)
+                    $dn = [system.String]::Join(",", $dn)
 
-                $cert = [System.Security.Cryptography.X509Certificates.X509Certificate]::new($certificatebinary)
-                $alt = " X509:<I>"+$cert.GetIssuerName()+"<S>"+$cert.GetName()
-                $alt = $alt.Split(",").Substring(1)
-                $alt = [system.String]::Join(",", $alt)
-                $alt2 = "X509:<I>DC=fi,DC=dextra,CN=PihlajalinnaCA<S>"+$dn
-                Get-ADUser -filter {employeeid -eq $employeeid} | set-aduser -Certificates @{Add=$cert} -Add @{'altSecurityIdentities'=$alt,$alt2; 'extensionattribute1'=$mail}
-            }catch{
-                if($error[0].Exception.InnerException.Message -like "*Exception calling *"){
-                    throw{
-                        "User $employeeid not found in AD"
-                    }
+                    $cert = [System.Security.Cryptography.X509Certificates.X509Certificate]::new($certificatebinary)
+                    $alt = " X509:<I>"+$cert.GetIssuerName()+"<S>"+$cert.GetName()
+                    $alt = $alt.Split(",").Substring(1)
+                    $alt = [system.String]::Join(",", $alt)
+                    $alt2 = "X509:<I>DC=fi,DC=dextra,CN=PihlajalinnaCA<S>"+$dn
+                                        
+                    Get-ADUser -filter {employeeid -eq $employeeid} | set-aduser -Certificates @{Add=$cert} -Add @{'altSecurityIdentities'=$alt,$alt2} -Replace @{'extensionattribute1'=$mail}
+                    Write-Host "Certificate installed to user $employeeid"
                 }
-            }
-        
-                Write-Host "Certificate installed to user $employeeid"
-                
             }
         }
-        }end{
-        Write-Host -NoNewLine 'Press any key to continue...';
-                $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
-                }
-                
+    }
+}end{
+    Write-Host -NoNewLine 'Press any key to continue...';
+    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+}
