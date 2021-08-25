1acdf99159ea5bbcd7abb8665073f544e2680f8e
Vilma Hallikas <42251794+VilmaH@users.noreply.github.com>
2021-08-25 11:42:28 +0300

    Update Optimize_OS_for_WVD.ps1

--- Optimize_OS_for_WVD.ps1
+++ Optimize_OS_for_WVD.ps1
@@ -44,7 +44,7 @@
  
  # run script
  # .\optimize -WindowsVersion 2004 -Verbose
-  .\Win10_VirtualDesktop_Optimize.ps1 -WindowsVersion 2004 -AcceptEULA:$true
+  .\Win10_VirtualDesktop_Optimize.ps1 -WindowsVersion 2004 -AcceptEULA:$true -Restart:$true
   write-host 'AIB Customization: Finished OS Optimizations script'
  
  
